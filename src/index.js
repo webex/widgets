e943f388724953c481871488a65c2156739edffd
Arash Koushkebaghi <arash.koush@gmail.com>
2020-02-07 11:18:02 -0800

    feat(App): import webex component system dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+// Testing bundle works with webex and component libraries
 import webex from 'webex';
+import WebexSDKAdapter from '@webex/sdk-component-adapter';
+import {WebexDataProvider, WebexMeeting} from '@webex/components';
