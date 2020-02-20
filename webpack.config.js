28bc89852d523ca3c2eb02c263e8fb4d8c5bef4a
Arash Koushkebaghi <arash.koush@gmail.com>
2020-02-20 12:01:48 -0800

    feat(webpack): add initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const webpack = require('webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  cache: true,
+  node: {
+    fs: 'empty', // Webex SDK `fs` dependency does not exist in browser.
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './demo'),
+    open: true,
+    overlay: true,
+    hot: true,
+    port: 9000,
+    stats: 'errors-warnings',
+    https: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin({}),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'demo/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
