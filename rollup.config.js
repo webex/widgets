59cef17b1b548f1b927a0547a6968b8fe7c6f20a
Arash Koushkebaghi <arash.koush@gmail.com>
2020-03-04 16:33:51 -0800

    build(rollup): add momentum-related build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonJS from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
 import postcss from 'rollup-plugin-postcss';
 
 const output = (name, format) => ({
@@ -31,9 +32,18 @@ export default [
         runtimeHelpers: true,
       }),
       commonJS(),
+      json(),
       postcss({extract: 'dist/webexWidgets.css'}),
     ],
+    onwarn(warning, warn) {
+      // skip circular dependency warnings from @momentum-ui/react library
+      if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+
+      // Use default for everything else
+      warn(warning);
+    },
     external: ['prop-types', 'react', 'react-dom', 'webex', '@webex/common'],
+    context: null,
   },
 ];
 9;
