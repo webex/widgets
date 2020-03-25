fb0b78a1f54f8172bae6cc6cf903ba464277e297
Adam Weeks <adweeks@cisco.com>
2020-03-25 15:28:24 -0400

    build(webpack): output to docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,69 +3,82 @@ const webpack = require('webpack');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  cache: true,
-  node: {
-    fs: 'empty', // Webex SDK `fs` dependency does not exist in browser.
-  },
-  devtool: 'inline-source-map',
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = function(env, argv) {
+  return {
+    cache: true,
+    output:
+      argv.mode === 'production'
+        ? {
+            path: path.resolve(__dirname, './docs'),
+            filename: 'demo.bundle.[hash].js',
+          }
+        : undefined, // Otherwise the CleanWebpackPlugin will wipe our build during devserver
+    node: {
+      fs: 'empty', // Webex SDK `fs` dependency does not exist in browser.
+    },
+    devtool: argv.mode === 'production' ? 'source-map' : 'inline-source-map',
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'assets/',
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
             },
-          },
-        ],
-      },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'assets/',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    devServer:
+      argv.mode === 'development'
+        ? {
+            contentBase: path.resolve(__dirname, './demo'),
+            open: true,
+            overlay: true,
+            hot: true,
+            port: 9000,
+            stats: 'errors-warnings',
+            https: true,
+          }
+        : undefined,
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'demo/index.html',
+        favicon: 'demo/teams.png',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
     ],
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './demo'),
-    open: true,
-    overlay: true,
-    hot: true,
-    port: 9000,
-    stats: 'errors-warnings',
-    https: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin({}),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'demo/index.html',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  };
 };
