afcc8cdccfa2a15e8e0381611b66bd81ebf969b9
Victor Malai <malai9696@gmail.com>
2021-04-20 22:11:30 +0300

    feat(WebexMeeting): test init user

--- test/specs/WebexMeeting/WebexMeeting.e2e.js
+++ test/specs/WebexMeeting/WebexMeeting.e2e.js
@@ -1,7 +1,14 @@
 import WebexMeetingPage from './WebexMeetingPage';
+import {createTestUser} from '@webex/test-users';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 // browser.waitUntil(() => {}, {timeout: 20000});
 describe('my widget', () => {
+  beforeAll(() => {
+    return WebexMeetingPage.initUser();
+  });
+
   it('should find sections and match text', () => {
     browser.url('https://webex.github.io/widgets/');
 
@@ -11,52 +18,52 @@ describe('my widget', () => {
     expect($('.content > section > h3')).toHaveTextContaining('Webex Widgets Access Token');
   });
 
-  it('should mute audio before joining meeting', () => {
-    WebexMeetingPage.init();
-
-    expect(WebexMeetingPage.microphoneIcon).not.toHaveElementClass('md-button--red');
-    WebexMeetingPage.microphoneIcon.click();
-    expect(WebexMeetingPage.microphoneIcon).toHaveElementClass('md-button--red');
-  });
-
-  it('should mute video before joining meeting', () => {
-    WebexMeetingPage.init();
-
-    expect(WebexMeetingPage.videoIcon).not.toHaveElementClass('md-button--red');
-    expect(WebexMeetingPage.videoRef).toExist();
-    WebexMeetingPage.videoIcon.click();
-    expect(WebexMeetingPage.videoIcon).toHaveElementClass('md-button--red');
-    expect(WebexMeetingPage.videoRef).not.toExist();
-  });
-
-  it('should mute audio after joining meeting', () => {
-    WebexMeetingPage.init();
-    expect(WebexMeetingPage.microphoneIcon).not.toHaveElementClass('md-button--red');
-
-    WebexMeetingPage.joinMeetingBtn.click();
-    WebexMeetingPage.waitJoiningMeeting();
-
-    WebexMeetingPage.microphoneIcon.click();
-    expect(WebexMeetingPage.microphoneIcon).toHaveElementClass('md-button--red');
-  });
-
-  it('should mute video after joining meeting', () => {
-    WebexMeetingPage.init();
-    expect(WebexMeetingPage.videoIcon).not.toHaveElementClass('md-button--red');
-    expect(WebexMeetingPage.videoRef).toExist();
-
-    WebexMeetingPage.joinMeetingBtn.click();
-    WebexMeetingPage.waitJoiningMeeting();
-
-    WebexMeetingPage.videoIcon.click();
-    expect(WebexMeetingPage.videoIcon).toHaveElementClass('md-button--red');
-    expect(WebexMeetingPage.videoRef).not.toExist();
-  });
-
-  it('should join meeting and waiting for others', () => {
-    WebexMeetingPage.init();
-    expect(WebexMeetingPage.waitingForOthers).not.toExist();
-    WebexMeetingPage.joinMeetingBtn.click();
-    expect(WebexMeetingPage.waitingForOthers).toExist();
-  });
+  // it('should mute audio before joining meeting', async () => {
+  //   WebexMeetingPage.init();
+  //   // console.log(2);
+  //   expect(WebexMeetingPage.microphoneIcon).not.toHaveElementClass('md-button--red');
+  //   WebexMeetingPage.microphoneIcon.click();
+  //   expect(WebexMeetingPage.microphoneIcon).toHaveElementClass('md-button--red');
+  // });
+  //
+  // it('should mute video before joining meeting', () => {
+  //   WebexMeetingPage.init();
+  //
+  //   expect(WebexMeetingPage.videoIcon).not.toHaveElementClass('md-button--red');
+  //   expect(WebexMeetingPage.videoRef).toExist();
+  //   WebexMeetingPage.videoIcon.click();
+  //   expect(WebexMeetingPage.videoIcon).toHaveElementClass('md-button--red');
+  //   expect(WebexMeetingPage.videoRef).not.toExist();
+  // });
+  //
+  // it('should mute audio after joining meeting', () => {
+  //   WebexMeetingPage.init();
+  //   expect(WebexMeetingPage.microphoneIcon).not.toHaveElementClass('md-button--red');
+  //
+  //   WebexMeetingPage.joinMeetingBtn.click();
+  //   WebexMeetingPage.waitJoiningMeeting();
+  //
+  //   WebexMeetingPage.microphoneIcon.click();
+  //   expect(WebexMeetingPage.microphoneIcon).toHaveElementClass('md-button--red');
+  // });
+  //
+  // it('should mute video after joining meeting', () => {
+  //   WebexMeetingPage.init();
+  //   expect(WebexMeetingPage.videoIcon).not.toHaveElementClass('md-button--red');
+  //   expect(WebexMeetingPage.videoRef).toExist();
+  //
+  //   WebexMeetingPage.joinMeetingBtn.click();
+  //   WebexMeetingPage.waitJoiningMeeting();
+  //
+  //   WebexMeetingPage.videoIcon.click();
+  //   expect(WebexMeetingPage.videoIcon).toHaveElementClass('md-button--red');
+  //   expect(WebexMeetingPage.videoRef).not.toExist();
+  // });
+  //
+  // it('should join meeting and waiting for others', () => {
+  //   WebexMeetingPage.init();
+  //   expect(WebexMeetingPage.waitingForOthers).not.toExist();
+  //   WebexMeetingPage.joinMeetingBtn.click();
+  //   expect(WebexMeetingPage.waitingForOthers).toExist();
+  // });
 });
--- test/specs/WebexMeeting/WebexMeetingPage.js
+++ test/specs/WebexMeeting/WebexMeetingPage.js
@@ -1,4 +1,18 @@
+import {createTestUser, removeTestUser, loginTestUser} from '@webex/test-users';
+import * as dotenv from 'dotenv';
+import {request} from '@webex/http-core';
+import _ from 'lodash';
+dotenv.config();
+
 class WebexMeetingPage {
+  constructor() {
+    this.user = null;
+  }
+
+  setUser(user) {
+    this.user = user;
+  }
+
   get accessTokenInput() {
     return $('#token');
   }
@@ -31,17 +45,42 @@ class WebexMeetingPage {
   }
 
   open() {
-    browser.url('https://webex.github.io/widgets/');
+    browser.url('https://localhost:9000/');
+  }
+
+  initUser() {
+    const payload = {
+      code: '123',
+      displayName: 'Test User',
+      clientId: process.env.WEBEX_CLIENT_ID,
+      clientSecret: process.env.WEBEX_CLIENT_SECRET,
+      idbrokerUrl: process.env.IDBROKER_BASE_URL,
+      cigServiceUrl: process.env.WEBEX_TEST_USERS_CI_GATEWAY_SERVICE_URL,
+      scopes: process.env.WEBEX_SCOPE,
+    };
+    console.log(payload);
+    return createTestUser(payload)
+      .then((data) => {
+        console.log(data);
+        // this.setUser(data);
+        // console.log(data);
+      })
+      .catch((e) => console.log('Error API', e));
   }
 
   init() {
+    // MzNiM2VkMTAtZGE4My00YjY3LThhMjktNGFlMmI0ZDEwNGM0MGNjNTA0MGMtOTU5_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f
+    // N2Y4NWUyZmItYTlhZC00YjMxLWE4NGQtZDc5MWE4Zjc4ODVkMmY3ZjlhZTYtYjBm_A52D_584cf4cd-eea7-4c8c-83ee-67d88fc6eab5
+    // console.log(this.user);
+    // console.log(this.user?.token?.access_token);
     this.open();
-    this.accessTokenInput.setValue('<INSERT ACCESS TOKEN>');
+    this.accessTokenInput.setValue(this.user?.token?.access_token);
     this.saveTokenBtn.click();
-    this.destinationInput.setValue('<INSERT email, person ID, room ID, SIP>');
+    this.destinationInput.setValue(this.user.email);
     this.displayWidgetBtn.click();
 
     this.waitLoadingMeeting();
+    // browser.waitUntil(() => {}, {timeout: 30000});
   }
 
   // Wait until interstitial meeting will appear
