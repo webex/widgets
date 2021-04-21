070025c574edd72540b3ff00f91ddb95d86c6db1
Victor Malai <malai9696@gmail.com>
2021-04-21 15:11:59 +0300

    feat(WebexMeeting): test widget

--- test/specs/WebexMeeting/WebexMeeting.e2e.js
+++ test/specs/WebexMeeting/WebexMeeting.e2e.js
@@ -1,3 +1,5 @@
+import WebexMeetingPage from './WebexMeetingPage';
+
 describe('my widget', () => {
   it('should find sections and match text', () => {
     browser.url('https://webex.github.io/widgets/');
@@ -6,5 +8,54 @@ describe('my widget', () => {
     expect($$('.content > section').length).toBe(2);
     // should match first section text
     expect($('.content > section > h3')).toHaveTextContaining('Webex Widgets Access Token');
+  });
+
+  it('should mute audio before joining meeting', () => {
+    WebexMeetingPage.init();
+
+    expect(WebexMeetingPage.microphoneIcon).not.toHaveElementClass('md-button--red');
+    WebexMeetingPage.microphoneIcon.click();
+    expect(WebexMeetingPage.microphoneIcon).toHaveElementClass('md-button--red');
+  });
+
+  it('should mute video before joining meeting', () => {
+    WebexMeetingPage.init();
+
+    expect(WebexMeetingPage.videoIcon).not.toHaveElementClass('md-button--red');
+    expect(WebexMeetingPage.videoRef).toExist();
+    WebexMeetingPage.videoIcon.click();
+    expect(WebexMeetingPage.videoIcon).toHaveElementClass('md-button--red');
+    expect(WebexMeetingPage.videoRef).not.toExist();
+  });
+
+  it('should mute audio after joining meeting', () => {
+    WebexMeetingPage.init();
+    expect(WebexMeetingPage.microphoneIcon).not.toHaveElementClass('md-button--red');
+
+    WebexMeetingPage.joinMeetingBtn.click();
+    WebexMeetingPage.waitJoiningMeeting();
+
+    WebexMeetingPage.microphoneIcon.click();
+    expect(WebexMeetingPage.microphoneIcon).toHaveElementClass('md-button--red');
+  });
+
+  it('should mute video after joining meeting', () => {
+    WebexMeetingPage.init();
+    expect(WebexMeetingPage.videoIcon).not.toHaveElementClass('md-button--red');
+    expect(WebexMeetingPage.videoRef).toExist();
+
+    WebexMeetingPage.joinMeetingBtn.click();
+    WebexMeetingPage.waitJoiningMeeting();
+
+    WebexMeetingPage.videoIcon.click();
+    expect(WebexMeetingPage.videoIcon).toHaveElementClass('md-button--red');
+    expect(WebexMeetingPage.videoRef).not.toExist();
+  });
+
+  it('should join meeting and waiting for others', () => {
+    WebexMeetingPage.init();
+    expect(WebexMeetingPage.waitingForOthers).not.toExist();
+    WebexMeetingPage.joinMeetingBtn.click();
+    expect(WebexMeetingPage.waitingForOthers).toExist();
   });
 });
