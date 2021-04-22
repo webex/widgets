1b3da11a2de5f5e8840ef9288fb52eaf633a4a23
Catalin Nicolae Lungan <lungancatalin@yahoo.com>
2021-04-22 17:11:28 +0300

    test(WebexMeeting): add e2e test for loading meeting widget

--- test/specs/WebexMeeting/WebexMeeting.e2e.js
+++ test/specs/WebexMeeting/WebexMeeting.e2e.js
@@ -1,10 +1,24 @@
-describe('my widget', () => {
-  it('should find sections and match text', () => {
-    browser.url('https://webex.github.io/widgets/');
+import MeetingPage from './MeetingPage';
+
+describe('Meeting Widget', () => {
+  beforeAll(() => {
+    return MeetingPage.createUser();
+  });
+
+  afterAll(() => {
+    return MeetingPage.removeUser();
+  });
+
+  it('opens meeting test page', () => {
+    MeetingPage.open();
 
     // should contain two sections
     expect($$('.content > section').length).toBe(2);
     // should match first section text
     expect($('.content > section > h3')).toHaveTextContaining('Webex Widgets Access Token');
+  });
+
+  it('loads meeting widget', () => {
+    MeetingPage.loadWidget();
   });
 });
