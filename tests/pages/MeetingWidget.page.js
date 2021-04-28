3c7538bae78a129ba972123c0e3090070bc53eb7
Catalin Nicolae Lungan <lungancatalin@yahoo.com>
2021-04-28 15:31:02 +0300

    test(WebexMeeting): e2e test mute video after join

--- tests/pages/MeetingWidget.page.js
+++ tests/pages/MeetingWidget.page.js
@@ -1,24 +1,18 @@
 class MeetingWidgetPage {
   get destination() { return $('input[placeholder="Widget Destination"]'); }
   get displayWidgetBtn() { return $('button[aria-label="Display Meeting Widget"]'); } // #md-button-3
-  get removeWidgetBtn() { return $('button[aria-label="Remove Meeting Widget"]'); } // #md-button-4
-  get meetingWidget() { return $('.meeting-widget'); };
   get interstitialMeeting() { return $('.wxc-interstitial-meeting'); }
   get meetingInfo() { return $('.wxc-meeting-info'); }
-  get muteAudioBtn() { return $('.meeting-controls-container button[alt=Mute]'); }
-  get unmuteAudioBtn() { return $('.meeting-controls-container button[alt=Unmute]'); }
-  get joinMeetingBtn() { return $('.meeting-controls-container button[alt="Join meeting"]'); }
+  get joinMeetingBtn() { return $('button=Join meeting'); }
   get waitingForOthers() { return $('h4=Waiting for others to join...'); }
+
+  get muteVideoBtn() { return $('.meeting-controls-container button[alt="Stop video"]'); }
+  get unmuteVideoBtn() { return $('.meeting-controls-container button[alt="Start video"]'); }  
 
   loadWidget(meetingDestination) {
     this.destination.setValue(meetingDestination);
     this.displayWidgetBtn.click();
     this.interstitialMeeting.waitForDisplayed({timeout: 30000});
-  }
-  
-  unloadWidget() {
-    this.removeWidgetBtn.click();
-    this.meetingWidget.waitForExist({reverse: true, timeout: 3000});
   }
 }
 
