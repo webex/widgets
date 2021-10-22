d4f99efbe8d1cd81a7ef545b968f0a3e6fda73e1
Mihai Cernaianu <mihai.cernaianu@endava.com>
2021-10-22 10:01:40 +0300

    fix(WebexMeeting): revive e2e tests

--- tests/pages/MeetingWidget.page.js
+++ tests/pages/MeetingWidget.page.js
@@ -1,18 +1,18 @@
 class MeetingWidgetPage {
   get destination() { return $('input[placeholder="Widget Destination"]'); }
-  get displayWidgetBtn() { return $('button[aria-label="Display Meeting Widget"]'); } // #md-button-3
-  get removeWidgetBtn() { return $('button[aria-label="Remove Meeting Widget"]'); } // #md-button-4
+  get displayWidgetBtn() { return $('button[aria-label="Display Meeting Widget"]'); }
+  get removeWidgetBtn() { return $('button[aria-label="Remove Meeting Widget"]'); }
   get meetingWidget() { return $('.webex-meeting-widget'); };
   get interstitialMeeting() { return $('.wxc-interstitial-meeting'); }
   get meetingInfo() { return $('.wxc-meeting-info'); }
-  get muteAudioBtn() { return $('.wxc-meeting-control-bar button[alt=Mute]'); }
-  get unmuteAudioBtn() { return $('.wxc-meeting-control-bar button[alt=Unmute]'); }
-  get muteVideoBtn() { return $('.wxc-meeting-control-bar button[alt="Stop video"]'); }
-  get unmuteVideoBtn() { return $('.wxc-meeting-control-bar button[alt="Start video"]'); }
-  get joinMeetingBtn() { return $('.wxc-meeting-control-bar button[alt="Join meeting"]'); }
-  get waitingForOthers() { return $('h4=Waiting for others to join...'); }
-  get leaveMeetingBtn() { return $('.wxc-meeting-control-bar button[alt=Leave]'); }
+  get waitingForOthers() { return $('h5=Waiting for others to join...'); }
   get controlBar() { return $('.wxc-meeting-control-bar'); }
+  get muteAudioBtn() { return this.controlBar.$('span=Mute'); }
+  get unmuteAudioBtn() { return this.controlBar.$('span=Unmute'); }
+  get muteVideoBtn() { return this.controlBar.$('span=Stop video'); }
+  get unmuteVideoBtn() { return this.controlBar.$('span=Start video'); }
+  get joinMeetingBtn() { return this.controlBar.$('span=Join meeting'); }
+  get leaveMeetingBtn() { return this.controlBar.$('.wxc-button--cancel'); }
 
   loadWidget(meetingDestination) {
     this.destination.setValue(meetingDestination);
