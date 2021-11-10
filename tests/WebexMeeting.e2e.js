97f7e46ce672bfe06914ee53ac862014cfdae6fb
Mihai Cernaianu <mihai.cernaianu@endava.com>
2021-11-10 10:00:02 +0200

    feat(tests): add join with media e2e test

--- tests/WebexMeeting.e2e.js
+++ tests/WebexMeeting.e2e.js
@@ -32,11 +32,14 @@ describe('Meeting Widget', () => {
     SamplesPage.open();
     SamplesPage.setAccessToken(accessToken);
     SamplesPage.navigateToMeetingPage();
-    MeetingPage.loadWidget(meetingDestination);
+    MeetingPage.destination.setValue(meetingDestination);
+
+    browser.execute(() => {
+      window.webexSDKAdapterSetLogLevel('debug');
+    });
   });
 
   afterAll(() => {
-    MeetingPage.unloadWidget();
     if (room) {
       browser.call(() => sdk.rooms.remove(room).catch(console.error));
     }
@@ -46,58 +49,105 @@ describe('Meeting Widget', () => {
     }
   });
 
-  it('has the correct page title', () => {
-    expect(SamplesPage.widgetTitle.getText()).toBe('Webex Meeting Widget');
+  describe('Join meeting with audio/video off', () => {
+    beforeAll(() => {
+      MeetingPage.loadWidget();
+    });
+
+    afterAll(() => {
+      MeetingPage.unloadWidget();
+    });
+
+    it('has the correct page title', () => {
+      expect(SamplesPage.widgetTitle.getText()).toBe('Webex Meeting Widget');
+    });
+
+    it('loads', () => {
+      expect(MeetingPage.meetingInfo).toBeDisplayed();
+    });
+
+    it('displays the control bar', () => {
+      expect(MeetingPage.controlBar).toBeVisible();
+    });
+
+    it('mutes audio before joining meeting', () => {
+      MeetingPage.muteAudioBtn.click();
+      expect(MeetingPage.muteAudioBtn).not.toBeVisible();
+      expect(MeetingPage.unmuteAudioBtn).toBeVisible();
+    });
+
+    it('mutes video before joining meeting', () => {
+      MeetingPage.muteVideoBtn.click();
+      expect(MeetingPage.muteVideoBtn).not.toBeVisible();
+      expect(MeetingPage.unmuteVideoBtn).toBeVisible();
+    });
+
+    it('displays "Waiting for others" after joining meeting', () => {
+      expect(MeetingPage.waitingForOthers).not.toExist();
+      MeetingPage.joinMeetingBtn.click();
+      MeetingPage.waitingForOthers.waitForDisplayed({timeout: 10000});
+      expect(MeetingPage.waitingForOthers).toBeVisible();
+    });
+
+    it('unmutes audio after joining meeting', () => {
+      MeetingPage.unmuteAudioBtn.click();
+      MeetingPage.muteAudioBtn.waitForDisplayed({timeout: 10000});
+      expect(MeetingPage.unmuteAudioBtn).not.toBeVisible();
+      expect(MeetingPage.muteAudioBtn).toBeVisible();
+    });
+
+    it('unmutes video after joining meeting', () => {
+      MeetingPage.unmuteVideoBtn.click();
+      MeetingPage.muteVideoBtn.waitForDisplayed({timeout: 10000});
+      expect(MeetingPage.unmuteVideoBtn).not.toBeVisible();
+      expect(MeetingPage.muteVideoBtn).toBeVisible();
+    });
+
+    it('leaves the meeting', () => {
+      MeetingPage.leaveMeetingBtn.click();
+      MeetingPage.meetingWidget.waitForDisplayed({timeout: 10000});
+      expect(MeetingPage.meetingWidget).toHaveTextContaining("You've successfully left the meeting");
+    });
+
+    it("doesn't display any control after leaving the meeting", () => {
+      expect(MeetingPage.controlBar).not.toBeVisible();
+    });
   });
 
-  it('loads', () => {
-    expect(MeetingPage.meetingInfo).toBeDisplayed();
-  });
+  describe('Join meeting with audio/video on', () => {
+    beforeAll(() => {
+      MeetingPage.loadWidget();
+    });
 
-  it('displays the control bar', () => {
-    expect(MeetingPage.controlBar).toBeVisible();
-  });
+    afterAll(() => {
+      MeetingPage.unloadWidget();
+    });
 
-  it('mutes audio before joining meeting', () => {
-    MeetingPage.muteAudioBtn.click();
-    expect(MeetingPage.muteAudioBtn).not.toBeVisible();
-    expect(MeetingPage.unmuteAudioBtn).toBeVisible();
-  });
+    it('displays "Waiting for others" after joining meeting', () => {
+      expect(MeetingPage.waitingForOthers).not.toExist();
+      MeetingPage.joinMeetingBtn.click();
+      MeetingPage.waitingForOthers.waitForDisplayed({timeout: 10000});
+      expect(MeetingPage.waitingForOthers).toBeVisible();
+    });
 
-  it('mutes video before joining meeting', () => {
-    MeetingPage.muteVideoBtn.click();
-    expect(MeetingPage.muteVideoBtn).not.toBeVisible();
-    expect(MeetingPage.unmuteVideoBtn).toBeVisible();
-  });
+    it('mutes audio after joining meeting', () => {
+      MeetingPage.muteAudioBtn.click();
+      MeetingPage.unmuteAudioBtn.waitForDisplayed({timeout: 10000});
+      expect(MeetingPage.muteAudioBtn).not.toBeVisible();
+      expect(MeetingPage.unmuteAudioBtn).toBeVisible();
+    });
 
-  it('displays "Waiting for others" after joining meeting', () => {
-    expect(MeetingPage.waitingForOthers).not.toExist();
-    MeetingPage.joinMeetingBtn.click();
-    MeetingPage.waitingForOthers.waitForDisplayed({timeout: 10000});
-    expect(MeetingPage.waitingForOthers).toBeVisible();
-  });
+    it('mutes video after joining meeting', () => {
+      MeetingPage.muteVideoBtn.click();
+      MeetingPage.unmuteVideoBtn.waitForDisplayed({timeout: 10000});
+      expect(MeetingPage.muteVideoBtn).not.toBeVisible();
+      expect(MeetingPage.unmuteVideoBtn).toBeVisible();
+    });
 
-  it('unmutes audio after joining meeting', () => {
-    MeetingPage.unmuteAudioBtn.click();
-    MeetingPage.muteAudioBtn.waitForDisplayed({timeout: 10000});
-    expect(MeetingPage.unmuteAudioBtn).not.toBeVisible();
-    expect(MeetingPage.muteAudioBtn).toBeVisible();
-  });
-
-  it('unmutes video after joining meeting', () => {
-    MeetingPage.unmuteVideoBtn.click();
-    MeetingPage.muteVideoBtn.waitForDisplayed({timeout: 10000});
-    expect(MeetingPage.unmuteVideoBtn).not.toBeVisible();
-    expect(MeetingPage.muteVideoBtn).toBeVisible();
-  });
-
-  it('leaves the meeting', () => {
-    MeetingPage.leaveMeetingBtn.click();
-    MeetingPage.meetingWidget.waitForDisplayed({timeout: 10000});
-    expect(MeetingPage.meetingWidget).toHaveTextContaining('You\'ve successfully left the meeting');
-  });
-
-  it('doesn\'t display any control after leaving the meeting', () => {
-    expect(MeetingPage.controlBar).not.toBeVisible();
+    it('leaves the meeting', () => {
+      MeetingPage.leaveMeetingBtn.click();
+      MeetingPage.meetingWidget.waitForDisplayed({timeout: 10000});
+      expect(MeetingPage.meetingWidget).toHaveTextContaining("You've successfully left the meeting");
+    });
   });
 });
