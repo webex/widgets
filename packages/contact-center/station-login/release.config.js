f4553c6cbfe566f413fdec2b06e08d57a373ad22
Shreyas Sharma <shreysh2@cisco.com>
2024-11-14 16:15:55 +0530

    feat(release): add-release-script-and-config

---
+++ packages/contact-center/station-login/release.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  branches: [
+    'master', // or 'main', depending on your main branch name
+    {
+      name: 'eft-pipeline',
+      prerelease: 'alpha', // This will trigger versions like 1.0.0-alpha.X
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+        },
+      },
+    ],
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
