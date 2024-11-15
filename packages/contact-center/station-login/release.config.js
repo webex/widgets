1220b98a880173ce35ac8e3e2ddcadf7b2e93d58
Shreyas Sharma <72344404+Shreyas281299@users.noreply.github.com>
2024-11-15 18:03:04 +0530

    fix(release): add-commit-analyzer (#283)
    Co-authored-by: Shreyas Sharma <shreysh2@cisco.com>

--- packages/contact-center/station-login/release.config.js
+++ packages/contact-center/station-login/release.config.js
@@ -9,6 +9,17 @@ module.exports = {
   ],
   plugins: [
     [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          {type: 'fix', release: 'patch'},
+          {type: 'feat', release: 'patch'},
+          {type: 'chore', release: 'patch'},
+        ],
+      },
+    ],
+    [
       'semantic-release-yarn',
       {
         yarnPublish: false,
