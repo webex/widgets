86e2820ff5cc66af09b6781f7319588ced4f420c
Shreyas Sharma <shreysh2@cisco.com>
2024-11-15 18:44:56 +0530

    fix(release): setup-and-then-release

---
+++ release.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: 'semantic-release-monorepo',
+  branches: [
+    'master',
+    {
+      name: 'eft-pipeline',
+      prerelease: 'alpha',
+    },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          {type: 'fix', release: 'patch'},
+          {type: 'feat', release: 'patch'},
+          {type: 'chore', release: 'patch'},
+        ],
+      },
+    ],
+    [
+      'semantic-release-yarn',
+      {
+        yarnPublish: false,
+      },
+    ],
+  ],
+  monorepo: {
+    forceRelease: true,
+  },
+  tagFormat: '${version}',
+};
