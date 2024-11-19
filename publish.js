7443890cadbf432808636201b9ab27bb6d9cbdbe
Shreyas Sharma <72344404+Shreyas281299@users.noreply.github.com>
2024-11-19 12:17:58 +0530

    fix(release): get-release-from-semantic-release (#310)
    Co-authored-by: Shreyas Sharma <shreysh2@cisco.com>

--- publish.js
+++ publish.js
@@ -1,31 +1,62 @@
 const {execSync} = require('child_process');
+const fs = require('fs');
 const path = require('path');
+
+// Function to remove the 'stableVersion' key
+function removeStableVersion(packageJsonPath) {
+  try {
+    const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf-8');
+    const packageData = JSON.parse(packageJsonContent);
+
+    if (packageData.hasOwnProperty('stableVersion')) {
+      delete packageData.stableVersion;
+
+      fs.writeFileSync(packageJsonPath, JSON.stringify(packageData, null, 2), 'utf-8');
+      console.log("'stableVersion' key removed successfully.");
+    } else {
+      console.log("'stableVersion' key does not exist in package.json.");
+    }
+  } catch (error) {
+    console.error("An error occurred while removing 'stableVersion':", error.message);
+  }
+}
 
 // Define the versionAndPublish function
 function versionAndPublish(workspaceName) {
   try {
-    let version;
     const branchName = process.argv[3];
+    const newVersion = process.argv[4];
+    const channelName = process.argv[5];
+    console.log(`This is the channel name: ${channelName}`);
 
-    try {
-      // Fetch the existing version from npm
-      version = execSync(`npm view ${workspaceName} dist-tags.${branchName}`, {stdio: 'pipe'})
-        .toString()
-        .trim();
-    } catch (npmError) {
-      // If no package is found (npm view fails), log a message and continue
-      console.log(`No existing version found for ${workspaceName}. Creating a new version.`);
-      version = '';
-    }
+    // try {
+    //   // Fetch the existing version from npm
+    //   version = execSync(`npm view ${workspaceName} dist-tags.${branchName}`, {stdio: 'pipe'})
+    //     .toString()
+    //     .trim();
+    // } catch (npmError) {
+    //   // If no package is found (npm view fails), log a message and continue
+    //   console.log(`No existing version found for ${workspaceName}. Creating a new version.`);
+    //   version = '';
+    // }
 
-    let newVersion;
-    if (!version) {
-      newVersion = `1.28.0-${branchName}.1`;
-    } else {
-      // Increment the patch number
-      const baseVersion = parseInt(version.split('.').pop(), 10);
-      newVersion = `1.28.0-${branchName}.${baseVersion + 1}`;
-    }
+    // Support minor/major release change
+
+    // let newVersion;
+    // if (!version) {
+    //   newVersion = `1.28.0-${branchName}.1`;
+    // } else {
+    //   // Increment the patch number
+    //   const baseVersion = parseInt(version.split('.').pop(), 10);
+    //   newVersion = `1.28.0-${branchName}.${baseVersion + 1}`;
+    // }
+
+    // https://github.com/yarnpkg/berry/issues/4328 - create JIRA ticket to fix this
+    console.log(`Removing stable version from package.json for ${workspaceName}`);
+
+    const packageName = workspaceName.split('/')[1].replace(/^cc-/, '');
+    const packageJsonPath = path.join(__dirname, `packages/contact-center/${packageName}`, 'package.json');
+    removeStableVersion(packageJsonPath);
 
     console.log(`Publishing new version for ${workspaceName}: ${newVersion}`);
 
@@ -50,5 +81,3 @@ if (!workspaceName) {
 
 // Call the versionAndPublish function with the workspace name
 versionAndPublish(workspaceName);
-
-module.exports = versionAndPublish;
