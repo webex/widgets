194ba2a7aef7d354aee9f118a531677bcd3353d8
Shreyas Sharma <shreysh2@cisco.com>
2024-12-02 14:43:26 +0530

    fix(cc-widgets): ship-all-widgets-together

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,15 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss'],
     fallback: {
-      fs:false,
+      fs: false,
       process: require.resolve('process/browser'),
-      crypto: require.resolve("crypto-browserify"),
-      querystring: require.resolve("querystring-es3"),
-      os: require.resolve("os-browserify/browser"),
-      stream: require.resolve("stream-browserify"),
-      vm: require.resolve("vm-browserify")
+      crypto: require.resolve('crypto-browserify'),
+      querystring: require.resolve('querystring-es3'),
+      os: require.resolve('os-browserify/browser'),
+      stream: require.resolve('stream-browserify'),
+      vm: require.resolve('vm-browserify'),
+      util: require.resolve('util/'),
+      url: require.resolve('url/'),
     },
   },
   plugins: [
