171eaf5d26fb9f0140501dacbbfd5c36f17a241f
parv_gour <parv_gour@PAGOUR-M-D8B2>
2024-12-25 14:49:45 +0530

    feat(cc): local changes

--- packages/contact-center/station-login/src/station-login/station-login.presentational.tsx
+++ packages/contact-center/station-login/src/station-login/station-login.presentational.tsx
@@ -1,66 +1,137 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect} from 'react';
 import {StationLoginPresentationalProps} from './station-login.types';
-import '@uuip/unified-ui-platform-common-components';
 
 const StationLoginPresentational: React.FunctionComponent<StationLoginPresentationalProps> = (props) => {
-  const {name, teams, loginOptions, login, logout, setDeviceType, setDialNumber, setTeam} = props;
-  console.log('props is', props);
-  console.log('teams is', teams);
-  console.log('loginOptions is', loginOptions);
-  const userProfileRef = useRef(null);
+  const {name, teams, loginOptions, login, logout, setDeviceType, setDialNumber, setTeam} = props; // TODO: Use the  loginSuccess, loginFailure, logoutSuccess props returned fromthe API response via helper file to reflect UI changes
 
-  // useEffect(() => {
-  //   console.log(props);
-  // }, [loginOptions, props]);
+  useEffect(() => {
+    const teamsDropdown = document.getElementById('teamsDropdown') as HTMLSelectElement;
+    const agentLogin = document.querySelector('#LoginOption') as HTMLSelectElement;
+    const dialNumber = document.querySelector('#dialNumber') as HTMLInputElement;
+    if (teamsDropdown) {
+      teamsDropdown.innerHTML = '';
+      if (teams) {
+        teams.forEach((team) => {
+          const option = document.createElement('option');
+          option.value = team.id;
+          option.text = team.name;
+          teamsDropdown.add(option);
+        });
+        setTeam(teamsDropdown.value);
+        dialNumber.value = '';
+        dialNumber.disabled = true;
+      }
+    }
+    if (loginOptions.length > 0) {
+      loginOptions.forEach((options)=> {
+        const option = document.createElement('option');
+        option.text = options;
+        option.value = options;
+        agentLogin.add(option);
+      });
+    }
+  }, [teams, loginOptions]);
 
-  // const selectLoginOption = (event: {target: {value: string}}) => {
-  //   const deviceType = event.target.value;
-  //   setDeviceType(deviceType);
-  // };
+  const selectLoginOption = (event: { target: { value: string; }; }) => {
+    const dialNumber = document.querySelector('#dialNumber') as HTMLInputElement;
+    const deviceType = event.target.value;
+    setDeviceType(deviceType);
+    if (deviceType === 'AGENT_DN' || deviceType === 'EXTENSION') {
+      dialNumber.disabled = false;
+    } else {
+      dialNumber.disabled = true;
+    }
+  };
 
-  // Below is a dummy representation of the new CC Widget UI, the below required props can be extracted from the props object and passed to the uuip-cc-user-profile Web Component.
+  function updateDN() {
+    const dialNumber = document.querySelector('#dialNumber') as HTMLInputElement;
+    setDialNumber(dialNumber.value);
+  }
 
-  const isEmergencyNotificationEnabled = true;
-  const isEmergencyNotificationAlreadyDisplayed = false;
-  const dialNumbers = ['123456001', '123456002', '123456003', '123456004'];
-  const extensions = ['201', '202', '203', '204'];
-  const isCallMonitoringEnabled = false;
-  const loginVoiceOptions = ['AGENT_DN', 'EXTENSION', 'BROWSER'];
-  const preferenceRoleName = 'agent_supervisor';
-  const roles = ['agent', 'supervisor'];
-  const teams1 = [
-    {teamId: '1', teamName: 'Team 1'},
-    {teamId: '2', teamName: 'Team 2'},
-  ];
+  const styles = {
+    box: {
+      backgroundColor: '#ffffff',
+      borderRadius: '8px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+      padding: '20px',
+      maxWidth: '800px',
+      margin: '0 auto'
+    },
 
-  const [isModalOpen, setIsModalOpen] = React.useState(true);
-  const [isSubmitBusy, setIsSubmitBusy] = React.useState(false);
+    sectionBox: {
+      padding: '10px',
+      border: '1px solid #ddd',
+      borderRadius: '8px'
+    },
+    
+    fieldset:  {
+      border: '1px solid #ccc',
+      borderRadius: '5px',
+      padding: '10px',
+      marginBottom: '20px'
+    },
+ 
+    legendBox: {
+      fontWeight: 'bold',
+      color: '#0052bf'
+    },
+
+    btn: {
+      padding: '10px 20px',
+      backgroundColor: '#0052bf',
+      color: 'white',
+      border: 'none',
+      borderRadius: '4px',
+      cursor: 'pointer',
+      transition: 'background-color 0.3s',
+      marginRight: '8px' 
+    },
+
+    select: {
+      width: '100%',
+      padding: '8px',
+      marginTop: '8px',
+      marginBottom: '12px',
+      border: '1px solid #ccc',
+      borderRadius: '4px'
+    },
+
+    input: {
+      width: '97%',
+      padding: '8px',
+      marginTop: '8px',
+      marginBottom: '12px',
+      border: '1px solid #ccc',
+      borderRadius: '4px'
+    }
+  }
 
   return (
-    <div>
-      <h3>This is the new CC Widget UI</h3>
-      <uuip-wc-user-station-login
-        isModalOpen={isModalOpen}
-        isRememberMeChecked={false}
-        isSubmitBusy={isSubmitBusy}
-        isDesktopEmergencyNotificationEnabled={isEmergencyNotificationEnabled}
-        isEmergencyNotificationAlreadyDisplayed={isEmergencyNotificationAlreadyDisplayed}
-        userRoles={roles}
-        teams={teams}
-        defaultTeam={teams[0]}
-        extensions={extensions}
-        isCallMonitoringEnabled={isCallMonitoringEnabled}
-        dialNumbers={dialNumbers}
-        defaultDialNumber={dialNumbers[0]}
-        loginVoiceOptions={loginVoiceOptions}
-        preferenceRoleName={preferenceRoleName}
-        signout-clicked={() => {
-          setIsModalOpen(false);
-          setIsSubmitBusy(false);
-        }}
-        confirm-clicked={(e) => setIsSubmitBusy(true)}
-      ></uuip-wc-user-station-login>
-    </div>
+    <><h1 data-testid="station-login-heading">{name}</h1>
+      <div style={styles.box}>
+      <section style={styles.sectionBox}>
+        <fieldset style={styles.fieldset}>
+        <legend style={styles.legendBox}>Agent</legend>
+          <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+            <div style={{ display: 'flex', gap: '1rem' }}>
+              <fieldset style={{border: '1px solid #ccc', borderRadius: '5px', padding: '10px', marginBottom: '20px', flex: 0.69 }}>
+                <legend style={styles.legendBox}>Select Team</legend>
+                <select id="teamsDropdown" style={styles.select}>Teams</select>
+              </fieldset>
+              <fieldset style={styles.fieldset}>
+                <legend style={styles.legendBox}>Agent Login</legend>
+                <select name="LoginOption" id="LoginOption" style={styles.select} onChange={selectLoginOption}>
+                  <option value="" selected hidden>Choose Agent Login Option...</option>
+                </select>
+                <input style={styles.input} id="dialNumber" name="dialNumber" placeholder="Extension/Dial Number" type="text" onInput={updateDN} />
+                <button id="AgentLogin" style={styles.btn} onClick={login}>Login</button>
+                <button id="logoutAgent" style={styles.btn} onClick={logout}>Logout</button>
+              </fieldset>
+            </div>
+          </div>
+        </fieldset> 
+      </section>
+    </div></>
   );
 };
 
