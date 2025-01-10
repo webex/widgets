0b304c451f6bc503278907bce3405ccd5cd5125f
Rajesh Kumar <131742425+rarajes2@users.noreply.github.com>
2025-01-10 12:05:32 +0530

    fix: cleanup and using mobx-react-lite (#356)

--- packages/contact-center/task/src/IncomingTask/index.tsx
+++ packages/contact-center/task/src/IncomingTask/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {observer} from 'mobx-react';
+import {observer} from 'mobx-react-lite';
 
 import store from '@webex/cc-store';
 import {useIncomingTask} from '../helper';
