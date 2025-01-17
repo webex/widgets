a0d12374f6118f634a5b2c96b45643216dc4d362
Shreyas Sharma <shreyassharma9912@gmail.com>
2025-01-17 12:40:23 +0530

    fix(call-control): add-web-component-for-call-control

--- packages/contact-center/cc-widgets/src/wc.ts
+++ packages/contact-center/cc-widgets/src/wc.ts
@@ -2,7 +2,7 @@ import r2wc from '@r2wc/react-to-web-component';
 import {StationLogin} from '@webex/cc-station-login';
 import {UserState} from '@webex/cc-user-state';
 import store from '@webex/cc-store';
-import {TaskList, IncomingTask} from '@webex/cc-task';
+import {TaskList, IncomingTask, CallControl} from '@webex/cc-task';
 
 const WebUserState = r2wc(UserState);
 const WebIncomingTask = r2wc(IncomingTask, {
@@ -26,6 +26,14 @@ const WebStationLogin = r2wc(StationLogin, {
   },
 });
 
+const WebCallControl = r2wc(CallControl, {
+  props: {
+    onHoldResume: 'function',
+    onEnd: 'function',
+    onWrapUp: 'function',
+  },
+});
+
 if (!customElements.get('widget-cc-user-state')) {
   customElements.define('widget-cc-user-state', WebUserState);
 }
@@ -40,6 +48,10 @@ if (!customElements.get('widget-cc-incoming-task')) {
 
 if (!customElements.get('widget-cc-task-list')) {
   customElements.define('widget-cc-task-list', WebTaskList);
+}
+
+if (!customElements.get('widget-cc-call-control')) {
+  customElements.define('widget-cc-call-control', WebCallControl);
 }
 
 export {store};
