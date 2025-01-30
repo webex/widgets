b55b8b2aed02aa8e989b991fb63f20d4deee1d9c
Shreyas Sharma <shreyassharma9912@gmail.com>
2025-01-30 14:05:54 +0530

    fix(samples): change samples index html hrefs

--- copy_to_docs.sh
+++ copy_to_docs.sh
@@ -10,9 +10,4 @@ cp ./widgets-samples/cc/samples-cc-wc-app/app.js docs/samples-cc-wc-app/
 cp ./widgets-samples/cc/samples-cc-wc-app/bundle.js docs/samples-cc-wc-app/
 cp ./widgets-samples/cc/samples-cc-wc-app/index.html docs/samples-cc-wc-app/
 
-# Update the hrefs in the copied index.html file
-sed -i '' 's|http://localhost:4000/|./samples-cc-wc-app/index.html|g' docs/index.html
-sed -i '' 's|http://localhost:3000/|./samples-cc-react-app/index.html|g' docs/index.html
-sed -i '' 's|https://localhost:9000/|./samples-meeting-app/index.html|g' docs/index.html
-
-echo "All dist folders, specific files from samples-cc-wc-app, and index.html from widgets-samples have been copied to the docs directory, and hrefs have been updated."
+echo "Samples created"
