690b942c4af4de59972e4a6265764400aa3532e5
Priya <pkesari@cisco.com>
2025-02-06 03:58:48 +0530

    fix(cc-react-app): webpack hmr implementation

--- widgets-samples/cc/samples-cc-react-app/webpack.config.js
+++ widgets-samples/cc/samples-cc-react-app/webpack.config.js
@@ -6,6 +6,34 @@ const baseConfig = require('../../../webpack.config');
 
 module.exports = merge(baseConfig, {
   entry: './src/index.tsx', // Entry file for bundling
+  resolve: {
+    alias: {
+      '@webex/cc-widgets': path.resolve(__dirname, '../../../packages/contact-center/cc-widgets/src'),
+      '@webex/cc-station-login': path.resolve(__dirname, '../../../packages/contact-center/station-login/src'),
+      '@webex/cc-user-state': path.resolve(__dirname, '../../../packages/contact-center/user-state/src'),
+      '@webex/cc-task': path.resolve(__dirname, '../../../packages/contact-center/task/src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        include: [path.resolve(__dirname, '../../../packages/contact-center/**/src')],
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.scss$/,
+        include: [path.join(__dirname, '../../../packages/contact-center/**/src')],
+        use: [
+          'style-loader', // Injects styles into DOM
+          'css-loader', // Turns CSS into CommonJS
+          'sass-loader', // Compiles Sass to CSS
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, '../../../docs/samples-cc-react-app'), // Output directory
     filename: 'bundle.js', // Output bundle file name
