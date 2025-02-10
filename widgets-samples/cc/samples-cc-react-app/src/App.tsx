2ff86cded87d89f052c79ac0fd06228b64abe03a
parv_gour <parv_gour@PAGOUR-M-D8B2>
2025-02-10 19:44:13 +0530

    feat(ccwidgets): added logs

--- widgets-samples/cc/samples-cc-react-app/src/App.tsx
+++ widgets-samples/cc/samples-cc-react-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useRef} from 'react';
 import {StationLogin, UserState, IncomingTask, TaskList, CallControl, store} from '@webex/cc-widgets';
-import {ThemeProvider,IconProvider} from '@momentum-design/components/dist/react';
+import {ThemeProvider, IconProvider} from '@momentum-design/components/dist/react';
 
 function App() {
   const [isSdkReady, setIsSdkReady] = useState(false);
@@ -8,13 +8,20 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const themeCheckboxRef = useRef(null);
   const [currentTheme, setCurrentTheme] = useState(store.currentTheme);
+  const [isMultiLoginEnabled, setIsMultiLoginEnabled] = useState(false);
 
   const webexConfig = {
     fedramp: false,
     logger: {
       level: 'log',
     },
+    cc: {
+      allowMultiLogin: isMultiLoginEnabled,
+      force: true,
+    },
   };
+
+  console.log('allowMultiLogin', isMultiLoginEnabled);
 
   const onLogin = () => {
     console.log('Agent login has been succesful');
@@ -54,52 +61,67 @@ function App() {
     console.log('onWrapup invoked');
   };
 
+  const enableMultiLogin = () => {
+    console.log('enableMultiLogin invoked');
+    setIsMultiLoginEnabled(true);
+  };
+
   return (
     <div className="mds-typography">
-      <ThemeProvider themeclass={currentTheme === 'LIGHT' ? 'mds-theme-stable-lightWebex': 'mds-theme-stable-darkWebex'}><IconProvider>
-        <h1>Contact Center widgets in a react app</h1>
-        <input
-          type="text"
-          placeholder="Enter your access token"
-          value={accessToken}
-          onChange={(e) => setAccessToken(e.target.value)}
-        />
-        <br />
-        <input
-          type='checkbox'
-          id='theme'
-          name='theme'
-          ref={themeCheckboxRef}
-          onChange={() => {
-            setCurrentTheme(themeCheckboxRef.current.checked ? 'DARK' : 'LIGHT');
-            store.setCurrentTheme(themeCheckboxRef.current.checked ? 'DARK' : 'LIGHT');
-          }}
-        /> Dark Theme
-        <br />
-        <button
-          disabled={accessToken.trim() === ''}
-          onClick={() => {
-            store.init({webexConfig, access_token: accessToken}).then(() => {
-              setIsSdkReady(true);
-            });
-          }}
-        >
-          Init Widgets
-        </button>
-        {isSdkReady && (
-          <>
-            <StationLogin onLogin={onLogin} onLogout={onLogout} />
-            {isLoggedIn && (
-              <>
-                <UserState />
-                <IncomingTask onAccepted={onAccepted} onDeclined={onDeclined} />
-                <TaskList onTaskAccepted={onTaskAccepted} onTaskDeclined={onTaskDeclined} />
-                <CallControl onHoldResume={onHoldResume} onEnd={onEnd} onWrapup={onWrapup} />
-              </>
-            )}
-          </>
-        )}
-      </IconProvider></ThemeProvider>
+      <ThemeProvider
+        themeclass={currentTheme === 'LIGHT' ? 'mds-theme-stable-lightWebex' : 'mds-theme-stable-darkWebex'}
+      >
+        <IconProvider>
+          <h1>Contact Center widgets in a react app</h1>
+          <input
+            type="text"
+            placeholder="Enter your access token"
+            value={accessToken}
+            onChange={(e) => setAccessToken(e.target.value)}
+          />
+          <br />
+          <input
+            type="checkbox"
+            id="theme"
+            name="theme"
+            ref={themeCheckboxRef}
+            onChange={() => {
+              setCurrentTheme(themeCheckboxRef.current.checked ? 'DARK' : 'LIGHT');
+              store.setCurrentTheme(themeCheckboxRef.current.checked ? 'DARK' : 'LIGHT');
+            }}
+          />{' '}
+          Dark Theme
+          <br />
+          <label>
+            <input type="checkbox" id="multiLoginFlag" name="multiLoginFlag" onChange={enableMultiLogin} /> Enable Multi
+            Login
+          </label>
+          <br />
+          <button
+            disabled={accessToken.trim() === ''}
+            onClick={() => {
+              store.init({webexConfig, access_token: accessToken}).then(() => {
+                setIsSdkReady(true);
+              });
+            }}
+          >
+            Init
+          </button>
+          {isSdkReady && (
+            <>
+              <StationLogin onLogin={onLogin} onLogout={onLogout} />
+              {isLoggedIn && (
+                <>
+                  <UserState />
+                  <IncomingTask onAccepted={onAccepted} onDeclined={onDeclined} />
+                  <TaskList onTaskAccepted={onTaskAccepted} onTaskDeclined={onTaskDeclined} />
+                  <CallControl onHoldResume={onHoldResume} onEnd={onEnd} onWrapup={onWrapup} />
+                </>
+              )}
+            </>
+          )}
+        </IconProvider>
+      </ThemeProvider>
     </div>
   );
 }
