d5a80349e5799d9f82b1eebc3712aa7fd3ead9ea
Shreyas Sharma <shreyassharma9912@gmail.com>
2025-02-12 20:31:59 +0530

    fix(store): task-end-workaround-wrapuprequired

--- packages/contact-center/store/src/storeEventsWrapper.ts
+++ packages/contact-center/store/src/storeEventsWrapper.ts
@@ -105,7 +105,9 @@ class StoreWrapper implements IStoreWrapper {
     const taskToRemove = this.store.taskList.find((task) => task.data.interactionId === taskId);
     if (taskToRemove) {
       taskToRemove.off(TASK_EVENTS.TASK_ASSIGNED, this.handleTaskAssigned(taskId));
-      taskToRemove.off(TASK_EVENTS.TASK_END, ({wrapupRequired}: {wrapupRequired: boolean}) => this.handleTaskEnd(taskId, wrapupRequired));
+      taskToRemove.off(TASK_EVENTS.TASK_END, ({wrapupRequired}: {wrapupRequired: boolean}) =>
+        this.handleTaskEnd(taskToRemove, wrapupRequired)
+      );
       taskToRemove.off(TASK_EVENTS.TASK_REJECT, () => this.handleTaskRemove(taskId));
     }
     const updateTaskList = this.store.taskList.filter((task) => task.data.interactionId !== taskId);
@@ -125,8 +127,14 @@ class StoreWrapper implements IStoreWrapper {
     });
   };
 
-  handleTaskEnd = (taskId: string, wrapupRequired: boolean) => {
-    this.store.setWrapupRequired(wrapupRequired);
+  handleTaskEnd = (task: ITask, wrapupRequired: boolean) => {
+    // TODO: SDK needs to send only 1 event on end : https://jira-eng-gpk2.cisco.com/jira/browse/SPARK-615785
+    if (task.data.interaction.state === 'connected') {
+      this.store.setWrapupRequired(true);
+      return;
+    } else if (task.data.interaction.state !== 'connected' && this.store.wrapupRequired !== true) {
+      this.handleTaskRemove(task.data.interactionId);
+    }
   };
 
   handleTaskAssigned = (task: ITask) => () => {
@@ -145,7 +153,7 @@ class StoreWrapper implements IStoreWrapper {
 
     // Attach event listeners to the task
     task.on(TASK_EVENTS.TASK_END, ({wrapupRequired}: {wrapupRequired: boolean}) => {
-      this.handleTaskEnd(task.data.interactionId, wrapupRequired);
+      this.handleTaskEnd(task, wrapupRequired);
     });
 
     // When we receive TASK_ASSIGNED the task was accepted by the agent and we need wrap up
