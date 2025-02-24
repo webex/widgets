cc5b70581745b7d08d7937441b3f2b5fd78c4643
Shreyas Sharma <72344404+Shreyas281299@users.noreply.github.com>
2025-02-24 12:32:18 +0530

    fix(tooling): Implement-HMR-in-react-samples-app (#392)

--- widgets-samples/cc/samples-cc-react-app/webpack.config.js
+++ widgets-samples/cc/samples-cc-react-app/webpack.config.js
@@ -6,6 +6,24 @@ const baseConfig = require('../../../webpack.config');
 
 module.exports = merge(baseConfig, {
   entry: './src/index.tsx', // Entry file for bundling
+  resolve: {
+    alias: {
+      '@webex/cc-store': path.resolve(__dirname, '../../../packages/contact-center/store/src'),
+      '@webex/cc-widgets': path.resolve(__dirname, '../../../packages/contact-center/cc-widgets/src'),
+      '@webex/cc-station-login': path.resolve(__dirname, '../../../packages/contact-center/station-login/src'),
+      '@webex/cc-user-state': path.resolve(__dirname, '../../../packages/contact-center/user-state/src'),
+      '@webex/cc-task': path.resolve(__dirname, '../../../packages/contact-center/task/src'),
+      '@webex/cc-components': path.resolve(__dirname, '../../../packages/contact-center/cc-components/src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, '../../../docs/samples-cc-react-app'), // Output directory
     filename: 'bundle.js', // Output bundle file name
@@ -23,5 +41,6 @@ module.exports = merge(baseConfig, {
     port: 3000, // Port for the dev server
     hot: true, // Enable hot module replacement
     open: false, // Open the app in browser on start
+    liveReload: true, // Reload page on changes
   },
 });
