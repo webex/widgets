cd209285aabe1c0d6298b7b2aa2df560a7a1adab
parv_gour <parv_gour@PAGOUR-M-D8B2>
2025-04-03 16:10:54 +0530

    feat(call-control): fix linting issues

--- packages/contact-center/task/src/CallControl/index.tsx
+++ packages/contact-center/task/src/CallControl/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { observer } from 'mobx-react-lite';
+import {observer} from 'mobx-react-lite';
 
 import store from '@webex/cc-store';
-import { useCallControl } from '../helper';
-import { CallControlProps } from '../task.types';
-import { CallControlComponentNew } from '@webex/cc-components';
+import {useCallControl} from '../helper';
+import {CallControlProps} from '../task.types';
+import {CallControlComponentNew} from '@webex/cc-components';
 
-const CallControl: React.FunctionComponent<CallControlProps> = observer(({ onHoldResume, onEnd, onWrapUp }) => {
-  const { logger, currentTask, deviceType, wrapupCodes, wrapupRequired } = store;
+const CallControl: React.FunctionComponent<CallControlProps> = observer(({onHoldResume, onEnd, onWrapUp}) => {
+  const {logger, currentTask, deviceType, wrapupCodes, wrapupRequired} = store;
   const result = {
-    ...useCallControl({ currentTask, deviceType, onHoldResume, onEnd, onWrapUp, logger }),
+    ...useCallControl({currentTask, deviceType, onHoldResume, onEnd, onWrapUp, logger}),
     wrapupRequired,
     wrapupCodes,
   };
@@ -17,4 +17,4 @@ const CallControl: React.FunctionComponent<CallControlProps> = observer(({ onHol
   return <CallControlComponentNew {...result} />;
 });
 
-export { CallControl };
+export {CallControl};
