9d03cb8c5c74d2c1619592eea6706805fc9ced82
Matthew Olker <molker@cisco.com>
2025-04-14 16:44:17 -0400

    [CAI-6149] Station Login UI Updates

--- packages/contact-center/cc-components/src/components/StationLogin/station-login.tsx
+++ packages/contact-center/cc-components/src/components/StationLogin/station-login.tsx
@@ -2,9 +2,9 @@ import React, {useEffect, useState, useRef, useCallback} from 'react';
 import {StationLoginComponentProps} from './station-login.types';
 import './station-login.style.scss';
 import {MULTIPLE_SIGN_IN_ALERT_MESSAGE, MULTIPLE_SIGN_IN_ALERT_TITLE} from './constants';
-import {ButtonPill, Text, SelectNext, TextInput} from '@momentum-ui/react-collaboration';
+import {ButtonPill, Text, SelectNext, TextInput, PopoverNext} from '@momentum-ui/react-collaboration';
 import {Item} from '@react-stately/collections';
-import {Icon} from '@momentum-design/components/dist/react';
+import {Icon, Select, Option} from '@momentum-design/components/dist/react';
 
 const StationLoginComponent: React.FunctionComponent<StationLoginComponentProps> = (props) => {
   const {
@@ -22,15 +22,16 @@ const StationLoginComponent: React.FunctionComponent<StationLoginComponentProps>
   } = props;
 
   const modalRef = useRef<HTMLDialogElement>(null);
+  const [dialNumberLabel, setDialNumberLabel] = useState<string>('');
+  const [dialNumberPlaceholder, setDialNumberPlaceholder] = useState<string>('');
   const [dialNumberValue, setDialNumberValue] = useState<string>('');
-  const [agentLoginValue, setAgentLoginValue] = useState<string>('');
   const [teamsValue, setTeamsValue] = useState<string>('');
   const [isDialNumberDisabled, setIsDialNumberDisabled] = useState<boolean>(false);
 
   useEffect(() => {
     if (loginOptions.length > 0) {
       const firstOption = loginOptions[0];
-      setAgentLoginValue('0');
+      selectLoginOption(firstOption);
       setDeviceType(firstOption);
     }
   }, [teams, loginOptions, deviceType]);
@@ -52,11 +53,10 @@ const StationLoginComponent: React.FunctionComponent<StationLoginComponentProps>
   const selectLoginOption = useCallback(
     (key: string) => {
       const index = parseInt(key, 10);
-      if (!isNaN(index) && loginOptions[index]) {
-        setAgentLoginValue(key);
-        setDeviceType(loginOptions[index]);
-        setIsDialNumberDisabled(!['AGENT_DN', 'EXTENSION'].includes(loginOptions[index]));
-      }
+      const selectedOption: string = !isNaN(index) ? loginOptions[index] : key;
+      setDeviceType(selectedOption);
+      setIsDialNumberDisabled(!['AGENT_DN', 'EXTENSION'].includes(selectedOption));
+      updateDialNumberLabel(selectedOption);
     },
     [loginOptions, setDeviceType]
   );
@@ -84,6 +84,16 @@ const StationLoginComponent: React.FunctionComponent<StationLoginComponentProps>
     [setTeam]
   );
 
+  const updateDialNumberLabel = (selectedOption: string): void => {
+    if (selectedOption === 'AGENT_DN') {
+      setDialNumberLabel('Dial number');
+      setDialNumberPlaceholder('Dial Number');
+    } else if (selectedOption === 'EXTENSION') {
+      setDialNumberLabel('Extension');
+      setDialNumberPlaceholder('Extension');
+    }
+  };
+
   return (
     <>
       <dialog ref={modalRef} className="modal" open={showMultipleLoginAlert}>
@@ -103,40 +113,52 @@ const StationLoginComponent: React.FunctionComponent<StationLoginComponentProps>
             </Text>
           </fieldset>
           <fieldset className="fieldset">
-            <legend id="agent-login-label">Handle calls using</legend>
-            <div className="select-container">
-              <SelectNext
+            <legend id="agent-login-label">
+              Handle calls using
+              <PopoverNext
+                trigger="mouseenter"
+                triggerComponent={<Icon name="info-badge-filled" id="agent-login-info-badge" />}
+                placement="auto-end"
+                closeButtonPlacement="top-left"
+                closeButtonProps={{'aria-label': 'Close'}}
+              >
+                <Text tagName={'div'} type="body-large-regular" className="agent-login-popover">
+                  This is your preferred method for receiving and making calls. Choose between your phone number,
+                  extension (if available), or your web browser.
+                </Text>
+              </PopoverNext>
+            </legend>
+            <div className="">
+              <Select
                 id="login-option"
-                direction="bottom"
-                showBorder
+                name="login-option"
                 aria-labelledby="agent-login-label"
-                items={loginOptions.map((name, id) => ({key: id.toString(), name}))}
-                selectedKey={agentLoginValue}
-                onSelectionChange={selectLoginOption}
+                onChange={(event: CustomEvent) => selectLoginOption(event.detail.value)}
                 className="station-login-select"
               >
-                {(item) => (
-                  <Item textValue={item.name} key={item.key}>
-                    <Text className="state-name" tagName={'small'}>
-                      {item.name}
-                    </Text>
-                  </Item>
-                )}
-              </SelectNext>
-              <Icon className="select-arrow-icon" name="arrow-down-bold" title="" />
+                {loginOptions.map((option, index) => (
+                  <Option key={index} value={option}>
+                    {option}
+                  </Option>
+                ))}
+              </Select>
             </div>
           </fieldset>
-          <fieldset className="fieldset">
-            <legend id="dial-number-label">Dial number</legend>
-            <TextInput
-              clearAriaLabel="Clear"
-              aria-labelledby="dial-number-label"
-              placeholder="Extension/Dial Number"
-              onChange={updateDN}
-              value={dialNumberValue}
-              isDisabled={isDialNumberDisabled}
-            />
-          </fieldset>
+          {isDialNumberDisabled ? (
+            <></>
+          ) : (
+            <fieldset className="fieldset">
+              <legend id="dial-number-label">{dialNumberLabel}</legend>
+              <TextInput
+                clearAriaLabel="Clear"
+                aria-labelledby="dial-number-label"
+                placeholder={dialNumberPlaceholder}
+                onChange={updateDN}
+                value={dialNumberValue}
+                isDisabled={isDialNumberDisabled}
+              />
+            </fieldset>
+          )}
           <fieldset className="fieldset">
             <legend id="team-label">Your team</legend>
             <div className="select-container">
@@ -163,11 +185,11 @@ const StationLoginComponent: React.FunctionComponent<StationLoginComponentProps>
           </fieldset>
           <div className="btn-container">
             {isAgentLoggedIn ? (
-              <ButtonPill id="logoutAgent" onPress={logout} color="cancel">
+              <ButtonPill id="logoutAgent" onPress={logout} color="join">
                 Logout
               </ButtonPill>
             ) : (
-              <ButtonPill id="AgentLogin" onPress={login} color="join">
+              <ButtonPill id="AgentLogin" onPress={login}>
                 Save & Continue
               </ButtonPill>
             )}
