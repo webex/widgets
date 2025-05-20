23f0708acf3cbe558ff42ae349fb0a06bb84546c
Shreyas Sharma <shreyassharma9912@gmail.com>
2025-05-20 11:45:30 +0530

    fix(tooling): remove webkit and firefox browser testing and remove retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,12 +20,10 @@ export default defineConfig({
     stderr: 'pipe',
   },
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -44,17 +42,17 @@ export default defineConfig({
       name: 'chromium',
       use: {...devices['Desktop Chrome']},
     },
+    // Once we have stability for playwright tests, we can enable the following browsers
+    // {
+    //   name: 'firefox',
+    //   use: {...devices['Desktop Firefox']},
+    //   dependencies: ['chromium'],
+    // },
 
-    {
-      name: 'firefox',
-      use: {...devices['Desktop Firefox']},
-      dependencies: ['chromium'],
-    },
-
-    {
-      name: 'webkit',
-      use: {...devices['Desktop Safari']},
-      dependencies: ['firefox'],
-    },
+    // {
+    //   name: 'webkit',
+    //   use: {...devices['Desktop Safari']},
+    //   dependencies: ['firefox'],
+    // },
   ],
 });
