c56b034e6cb3bdfd76c6fa3cf4f8a7fdca57854f
Shreyas Sharma <shreyassharma9912@gmail.com>
2025-05-20 12:17:56 +0530

    fix(tooling): add relogin e2e test

--- playwright/login-user-state.spec.ts
+++ playwright/login-user-state.spec.ts
@@ -8,11 +8,11 @@ test.describe('Login and User-State testing', () => {
       throw new Error('ACCESS_TOKEN is not defined in the environment variables');
     }
   });
-  test('should login using Extension login option', async ({page}) => {
+  test('Extension Login: should login using Extension login option', async ({page}) => {
     await page.goto('http://localhost:3000/');
-    await page.getByText('Contact Center widgets in a react app Dark Theme Select Widgets to Show Station').click();
-    await page.getByRole('textbox', {name: 'Enter your access token'}).click();
-    if (accessToken) await page.getByRole('textbox', {name: 'Enter your access token'}).fill(accessToken);
+    await page.getByRole('textbox').click();
+    if (accessToken) await page.getByRole('textbox').fill(accessToken);
+
     await page.getByRole('checkbox', {name: 'Enable Multi Login'}).check();
     await page.getByRole('button', {name: 'Init Widgets'}).click();
 
@@ -47,7 +47,7 @@ test.describe('Login and User-State testing', () => {
     await expect(page.getByTestId('login-button')).toContainText(/Save & Continue\s*/i);
   });
 
-  test('should login accross tabs', async ({browser}) => {
+  test('Multilogin: should login accross tabs', async ({browser}) => {
     const context = await browser.newContext();
 
     const page = await context.newPage();
@@ -55,14 +55,14 @@ test.describe('Login and User-State testing', () => {
 
     await page.goto('http://localhost:3000/');
     await page2.goto('http://localhost:3000/');
-    await page.getByRole('textbox', {name: 'Enter your access token'}).click();
-    await page2.getByRole('textbox', {name: 'Enter your access token'}).click();
+    await page.getByRole('textbox').click();
+    await page2.getByRole('textbox').click();
 
     if (!process.env.ACCESS_TOKEN) {
       throw new Error('ACCESS_TOKEN is not defined in the environment variables');
     }
-    await page.getByRole('textbox', {name: 'Enter your access token'}).fill(process.env.ACCESS_TOKEN);
-    await page2.getByRole('textbox', {name: 'Enter your access token'}).fill(process.env.ACCESS_TOKEN);
+    await page.getByRole('textbox').fill(process.env.ACCESS_TOKEN);
+    await page2.getByRole('textbox').fill(process.env.ACCESS_TOKEN);
 
     await page.getByRole('checkbox', {name: 'Enable Multi Login'}).check();
     await page2.getByRole('checkbox', {name: 'Enable Multi Login'}).check();
@@ -74,7 +74,7 @@ test.describe('Login and User-State testing', () => {
     await page2.getByTestId('station-login-widget').waitFor({state: 'visible'});
 
     if (await page.getByTestId('login-button').isVisible()) {
-      await expect(page.getByTestId('login-button')).toContainText('Save & dont Continue');
+      await expect(page.getByTestId('login-button')).toContainText(/Save & Continue\s*/i);
     } else if (await page.getByTestId('logout-button').isVisible()) {
       await expect(page.getByTestId('logout-button')).toContainText(/Sign out\s*/i);
       await page.getByTestId('logout-button').click();
@@ -93,6 +93,53 @@ test.describe('Login and User-State testing', () => {
     await page2.getByTestId('state-select').click();
     await page2.getByTestId('state-item-Available').click();
     await expect(page.getByTestId('state-name')).toContainText('Available');
+    await page.getByTestId('logout-button').click();
+    await expect(page.getByTestId('login-button')).toContainText(/Save & Continue\s*/i);
+  });
+
+  test('Relogin: should login after a refresh with same deviceType', async ({browser}) => {
+    const context = await browser.newContext();
+
+    const page = await context.newPage();
+
+    await page.goto('http://localhost:3000/');
+    await page.getByRole('textbox').click();
+
+    if (!process.env.ACCESS_TOKEN) {
+      throw new Error('ACCESS_TOKEN is not defined in the environment variables');
+    }
+    await page.getByRole('textbox').fill(process.env.ACCESS_TOKEN);
+
+    await page.getByRole('button', {name: 'Init Widgets'}).click();
+
+    await page.getByTestId('station-login-widget').waitFor({state: 'visible'});
+
+    if (await page.getByTestId('login-button').isVisible()) {
+      await expect(page.getByTestId('login-button')).toContainText(/Save & Continue\s*/i);
+    } else if (await page.getByTestId('logout-button').isVisible()) {
+      await expect(page.getByTestId('logout-button')).toContainText(/Sign out\s*/i);
+      await page.getByTestId('logout-button').click();
+      await expect(page.getByTestId('login-button')).toContainText(/Save & Continue\s*/i);
+    }
+
+    await page.getByTestId('login-option-select').click();
+    await page.getByTestId('login-option-Extension').click();
+    await page.getByTestId('dial-number-input').getByRole('textbox').fill('1234');
+    await page.getByTestId('login-button').click();
+    await expect(page.getByTestId('logout-button')).toContainText(/Sign out\s*/i);
+
+    await page.reload();
+
+    await page.getByRole('textbox').click();
+    if (!process.env.ACCESS_TOKEN) {
+      throw new Error('ACCESS_TOKEN is not defined in the environment variables');
+    }
+    await page.getByRole('textbox').fill(process.env.ACCESS_TOKEN);
+    await page.getByRole('button', {name: 'Init Widgets'}).click();
+    await page.getByTestId('station-login-widget').waitFor({state: 'visible'});
+
+    await expect(page.getByTestId('login-option-select').locator('#select-base-triggerid')).toContainText('Extension');
+    await expect(page.getByTestId('state-name')).toContainText('Meeting');
     await page.getByTestId('logout-button').click();
     await expect(page.getByTestId('login-button')).toContainText(/Save & Continue\s*/i);
   });
