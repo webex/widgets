7c4a02197835b09886b5c566651aa10f38f34cb5
Shreyas Sharma <72344404+Shreyas281299@users.noreply.github.com>
2025-05-26 15:12:44 +0530

    fix(tooling): fix jest setup for UI testing (#460)

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  rootDir: '.',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss)$': 'babel-jest',
+  },
+  testEnvironment: 'jsdom',
+  testMatch: ['**/tooling/tests/**/*.js'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@momentum-design/components|@momentum-ui/react-collaboration|@lit|lit|cheerio))',
+  ],
+  // Use babel-jest or ts-jest depending on your setup
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+    '\\.[jt]s?$': 'babel-jest',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+};
