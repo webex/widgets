b8a5b64cc1ec44a9079e55a696bbd51fa3317d55
PrayagGP <prayagganeshprabhu.221cs140@nitk.edu.in>
2025-06-18 23:03:30 +0530

    fix(cc-widgets): add a constants file

---
+++ playwright/constants.ts
@@ -0,0 +1,21 @@
+export const BASE_URL = 'http://localhost:3000';
+
+// State constants
+export const MEETING = 'Meeting';
+export const AVAILABLE = 'Available';
+export const LUNCH = 'Lunch';
+
+// Aux code ID constants
+export const AVAILABLE_ID = '0';
+export const MEETING_ID = 'c0831638-c5f8-4eed-9100-2751c94abbea';
+export const LUNCH_ID = '0f0cd0ca-4a19-4773-b18f-0215abe03fad';
+
+// Theme color constants
+export const GREEN = 'rgb(206, 245, 235)';
+export const GREY = 'rgba(0, 0, 0, 0.11)';
+
+// Timeout constants
+export const LONG_WAIT = 40000;
+
+
+
