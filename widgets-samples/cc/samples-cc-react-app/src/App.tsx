67ec2452c9719418fd4c51db59b0a442e738797a
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2025-07-04 10:44:50 +0530

    fix(cc-samples): refresh page onCCSignout

--- widgets-samples/cc/samples-cc-react-app/src/App.tsx
+++ widgets-samples/cc/samples-cc-react-app/src/App.tsx
@@ -129,6 +129,7 @@ const handleSaveEnd = (isComplete: boolean) => {
 
   const onCCSignOut = () => {
     console.log('CC Sign out has been successful');
+    window.location.href = window.location.href;
   };
 
   const onAccepted = ({task}) => {
