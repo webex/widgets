74b01b9340d3188a5f89b21e7ba26a705d70dfb5
Ravi Chandra Sekhar Sarika <rsarika@cisco.com>
2025-07-09 22:55:22 +0530

    fix: updated config to embed engage

--- packages/contact-center/cc-digital-channels/vite.config.ts
+++ packages/contact-center/cc-digital-channels/vite.config.ts
@@ -87,11 +87,10 @@ export default createViteConfig({
   entry: 'src/index.ts',
   libName: 'MinimalWebexEngageApp',
   port: 3241,
-  external: ['react', 'react-dom', '@webex-engage/wxengage-conversations', 'react/jsx-runtime', '@webex/cc-store'],
+  external: ['react', 'react-dom', 'react/jsx-runtime', '@webex/cc-store'],
   globals: {
     react: 'React',
     'react-dom': 'ReactDOM',
     'react/jsx-runtime': 'React',
-    '@webex-engage/wxengage-conversations': 'WebexEngage',
   },
 });
