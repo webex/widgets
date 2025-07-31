9b5c9ab9fb5766b44f96c95056b1bc7ed774d69e
Ravi Chandra Sekhar Sarika <rsarika@cisco.com>
2025-07-31 14:14:53 +0530

    fix: switch between tasks

--- packages/contact-center/cc-digital-channels/src/digital-channels/index.tsx
+++ packages/contact-center/cc-digital-channels/src/digital-channels/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import store from '@webex/cc-store';
 import {observer} from 'mobx-react-lite';
 import Engage from '@webex-engage/wxengage-conversations';
@@ -6,6 +6,7 @@ import Engage from '@webex-engage/wxengage-conversations';
 import {useDigitalChannels} from '../helper';
 import {DigitalChannelsProps} from './digital-channels.types';
 import '@momentum-ui/web-components';
+
 const DigitalChannels: React.FunctionComponent<DigitalChannelsProps> = observer(({jwtToken, dataCenter, onError}) => {
   const {logger, currentTask} = store;
 
@@ -23,10 +24,22 @@ const DigitalChannels: React.FunctionComponent<DigitalChannelsProps> = observer(
 
   const {handleError, conversationId} = result;
 
+  // Create a stable key based on critical props to force remount when they change
+  // This prevents issues with the Froala editor trying to cleanup/reinitialize improperly
+  const componentKey = useMemo(() => {
+    return `${conversationId}-${jwtToken.slice(-8)}-${dataCenter}`;
+  }, [conversationId, jwtToken, dataCenter]);
+
   return (
     <div>
       <md-theme id="app-theme" theme="momentumV2" class="is-visual-rebrand">
-        <Engage conversationId={conversationId} jwtToken={jwtToken} dataCenter={dataCenter} onError={handleError} />
+        <Engage
+          key={componentKey}
+          conversationId={conversationId}
+          jwtToken={jwtToken}
+          dataCenter={dataCenter}
+          onError={handleError}
+        />
       </md-theme>
     </div>
   );
