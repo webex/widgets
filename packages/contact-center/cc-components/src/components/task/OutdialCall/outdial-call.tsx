a2cb2859b30056a86d902efb4503eedbf0bca780
Rajesh Kumar <rarajes2@cisco.com>
2025-12-02 23:24:45 +0530

    feat(contact-center): add address book search to outdial

--- packages/contact-center/cc-components/src/components/task/OutdialCall/outdial-call.tsx
+++ packages/contact-center/cc-components/src/components/task/OutdialCall/outdial-call.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useMemo, useState} from 'react';
-import {OutdialAniEntry, OutdialCallComponentProps} from '../task.types';
-import './outdial-call.style.scss';
 import {withMetrics} from '@webex/cc-ui-logging';
-import {Input, Button, Option, Select} from '@momentum-design/components/dist/react';
+import {Input, Button, Option, Select, Tab, TabList} from '@momentum-design/components/dist/react';
+
+import {OutdialAniEntry, OutdialCallComponentProps} from '../task.types';
 import {OutdialStrings, KEY_LIST} from './constants';
+
+import './outdial-call.style.scss';
 
 /**
  * OutdialCallComponent renders a dialpad UI for agents to initiate outbound calls.
@@ -18,7 +20,13 @@ import {OutdialStrings, KEY_LIST} from './constants';
 const OutdialCallComponent: React.FunctionComponent<OutdialCallComponentProps> = (props) => {
   const {logger, startOutdial, getOutdialANIEntries} = props;
 
+  const TABS = {
+    DIAL_PAD: 'dial_pad',
+    ADDRESS_BOOK: 'address_book',
+  };
+
   // State Hooks
+  const [selectedTab, setSelectedTab] = useState(TABS.DIAL_PAD);
   const [destination, setDestination] = useState('');
   const [isValidNumber, setIsValidNumber] = useState('');
   const [selectedANI, setSelectedANI] = useState(undefined);
@@ -71,32 +79,76 @@ const OutdialCallComponent: React.FunctionComponent<OutdialCallComponentProps> =
     validateOutboundNumber(destination + value);
   };
 
+  const renderAddressBook = () => {
+    return (
+      <section className="address-book" data-testid="outdial-address-book-container">
+        <p>Address book panel</p>
+      </section>
+    );
+  };
+
+  const renderDiapad = () => {
+    return (
+      <section className="keypad" data-testid="outdial-call-container">
+        <Input
+          className="outdial-input"
+          id="outdial-number-input"
+          name="outdial-number-input"
+          data-testid="outdial-number-input"
+          helpText={isValidNumber}
+          helpTextType={isValidNumber ? 'error' : 'default'}
+          placeholder={OutdialStrings.DN_PLACEHOLDER}
+          value={destination}
+          onChange={(e: unknown) => {
+            const inputValue = (e as React.ChangeEvent<HTMLInputElement>).target.value;
+            setDestination(inputValue);
+            validateOutboundNumber(inputValue);
+          }}
+        />
+        <ul className="keys" data-testid="outdial-keypad-keys">
+          {KEY_LIST.map((key) => (
+            <li key={key}>
+              <Button className="key button" onClick={() => handleOnClick(key)}>
+                {key}
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  };
+
   return (
-    <article className="keypad" data-testid="outdial-call-container">
-      <Input
-        className="outdial-input"
-        id="outdial-number-input"
-        name="outdial-number-input"
-        data-testid="outdial-number-input"
-        helpText={isValidNumber}
-        helpTextType={isValidNumber ? 'error' : 'default'}
-        placeholder={OutdialStrings.DN_PLACEHOLDER}
-        value={destination}
-        onChange={(e: unknown) => {
-          const inputValue = (e as React.ChangeEvent<HTMLInputElement>).target.value;
-          setDestination(inputValue);
-          validateOutboundNumber(inputValue);
-        }}
-      />
-      <ul className="keys" data-testid="outdial-keypad-keys">
-        {KEY_LIST.map((key) => (
-          <li key={key}>
-            <Button className="key button" onClick={() => handleOnClick(key)}>
-              {key}
-            </Button>
-          </li>
-        ))}
-      </ul>
+    <article className="outdial-container">
+      <TabList activeTabId={selectedTab} dataAriaLabel="Outdial call tabs" className="tab-list">
+        <Tab
+          iconName="contact-card-bold"
+          tabId={TABS.ADDRESS_BOOK}
+          aria-controls={TABS.ADDRESS_BOOK}
+          variant="glass"
+          onClick={() => setSelectedTab(TABS.ADDRESS_BOOK)}
+        ></Tab>
+
+        <Tab
+          iconName="dialpad-bold"
+          tabId={TABS.DIAL_PAD}
+          aria-controls={TABS.DIAL_PAD}
+          variant="glass"
+          onClick={() => setSelectedTab(TABS.DIAL_PAD)}
+        ></Tab>
+      </TabList>
+
+      {selectedTab === TABS.ADDRESS_BOOK && (
+        <div id={TABS.ADDRESS_BOOK} role="tabpanel" aria-labelledby={TABS.ADDRESS_BOOK}>
+          {renderAddressBook()}
+        </div>
+      )}
+      {selectedTab === TABS.DIAL_PAD && (
+        <div id={TABS.DIAL_PAD} role="tabpanel" aria-labelledby={TABS.DIAL_PAD}>
+          {renderDiapad()}
+        </div>
+      )}
+
       <Select
         className="outdial-input"
         label={OutdialStrings.ANI_SELECT_LABEL}
