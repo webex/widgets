bb3bea7f90be652b3c163da9aa19e34fd346244f
Ritesh Singh <133033102+riteshfyi@users.noreply.github.com>
2026-04-08 15:46:14 +0530

    fix(meetings): address version resolution (#664)

--- widgets-samples/samples-meeting-app/webpack.config.js
+++ widgets-samples/samples-meeting-app/webpack.config.js
@@ -4,6 +4,8 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {version} = require('./package.json');
 
+const widgetsDir = path.dirname(require.resolve('@webex/widgets/package.json'));
+
 module.exports = function(env, argv) {
   return {
     cache: true,
@@ -18,6 +20,13 @@ module.exports = function(env, argv) {
     devtool: argv.mode === 'production' ? 'source-map' : 'inline-source-map',
     resolve: {
       extensions: ['.js', '.jsx'],
+      alias: {
+        react: path.dirname(require.resolve('react/package.json', { paths: [widgetsDir] })),
+        'react-dom': path.dirname(require.resolve('react-dom/package.json', { paths: [widgetsDir] })),
+        'prop-types': path.dirname(require.resolve('prop-types/package.json', { paths: [widgetsDir] })),
+        webex: path.dirname(require.resolve('webex/package', { paths: [widgetsDir] })),
+        '@webex/common': path.dirname(require.resolve('@webex/common/package.json', { paths: [widgetsDir] })),
+      },
       fallback: {
         "buffer": require.resolve("buffer/"),
         "crypto": require.resolve("crypto-browserify"),
