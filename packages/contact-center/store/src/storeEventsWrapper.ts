54954e9ec5d15fa0c8a01a25884b69f9134f678a
Zach Raymer <zraymer@cisco.com>
2026-06-15 15:54:04 -0500

    chore: set default values for inactivity properties

--- packages/contact-center/store/src/storeEventsWrapper.ts
+++ packages/contact-center/store/src/storeEventsWrapper.ts
@@ -418,8 +418,8 @@ class StoreWrapper implements IStoreWrapper {
         orgId: profile.orgId || undefined,
         roles: profile.roles || undefined,
         deviceType: profile.deviceType || undefined,
-        isTimeoutDesktopInactivityEnabled: profile.isTimeoutDesktopInactivityEnabled,
-        timeoutDesktopInactivityMins: profile.timeoutDesktopInactivityMins,
+        isTimeoutDesktopInactivityEnabled: profile.isTimeoutDesktopInactivityEnabled || undefined,
+        timeoutDesktopInactivityMins: profile.timeoutDesktopInactivityMins || undefined,
       };
     });
   };
